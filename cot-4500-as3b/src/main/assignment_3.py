1f054ffd48e71fb4464d000514f7725692018a5e
JaydeLynch <jaydelynch104@gmail.com>
2025-04-04 15:11:26 -0400

    Update assignment_3.py

--- cot-4500-as3b/src/main/assignment_3.py
+++ cot-4500-as3b/src/main/assignment_3.py
@@ -1,43 +1,25 @@
 import numpy as np
 
-def question1():
-    # Solve system using Gaussian elimination
-    A = np.array([[2, -1, 1],
-                  [1, 3, 1],
-                  [-1, 5, 4]], dtype=float)
-    b = np.array([6, 0, -3], dtype=float)
-    
-    # Augmented matrix
-    aug = np.column_stack((A, b))
-    
-    # Forward elimination
+def gaussian_elimination(A, b):
     n = len(b)
-    for i in range(n):
-        # Partial pivoting
-        max_row = np.argmax(abs(aug[i:, i])) + i
-        aug[[i, max_row]] = aug[[max_row, i]]
-        
-        # Elimination
-        for j in range(i+1, n):
-            factor = aug[j, i] / aug[i, i]
-            aug[j, i:] -= factor * aug[i, i:]
+    aug_matrix = np.hstack([A, b.reshape(-1, 1)])
     
-    # Back substitution
+    # Partial pivoting
+    for i in range(n):
+        max_row = np.argmax(abs(aug_matrix[i:, i])) + i
+        aug_matrix[[i, max_row]] = aug_matrix[[max_row, i]]
+        
+        for j in range(i+1, n):
+            factor = aug_matrix[j][i] / aug_matrix[i][i]
+            aug_matrix[j] = aug_matrix[j] - factor * aug_matrix[i]
+    
     x = np.zeros(n)
     for i in range(n-1, -1, -1):
-        x[i] = (aug[i, -1] - np.dot(aug[i, i+1:n], x[i+1:n])) / aug[i, i]
+        x[i] = (aug_matrix[i][-1] - np.dot(aug_matrix[i][i+1:n], x[i+1:n])) / aug_matrix[i][i]
     
-    print(x[0])
-    print(x[1])
-    print(x)
+    return x
 
-def question2():
-    # LU factorization
-    A = np.array([[1, 1, 0, 3],
-                  [2, 1, -1, 1],
-                  [3, -1, -1, 2],
-                  [-1, 2, 3, -1]], dtype=float)
-    
+def lu_factorization(A):
     n = A.shape[0]
     L = np.eye(n)
     U = A.copy()
@@ -47,48 +29,29 @@ def question2():
             L[i, k] = U[i, k] / U[k, k]
             U[i, k:] -= L[i, k] * U[k, k:]
     
-    # Determinant
-    det = np.prod(np.diag(U))
-    print(det)
-    
-    # L matrix
-    print(L)
-    
-    # U matrix
-    print(U)
+    return L, U
 
-def question3():
-    # Check diagonal dominance
-    A = np.array([[9, 0, 5, 2, 1],
-                  [3, 9, 1, 2, 1],
-                  [0, 1, 7, 2, 3],
-                  [4, 2, 3, 12, 2],
-                  [3, 2, 4, 0, 8]])
-    
-    diag = np.diag(np.abs(A))
-    off_diag = np.sum(np.abs(A), axis=1) - diag
-    is_dom = np.all(diag >= off_diag)
-    print(is_dom)
+# Question 1
+A1 = np.array([[2, -1, 1], [1, 3, 1], [-1, 5, 4]], dtype=float)
+b1 = np.array([6, 0, -3], dtype=float)
+solution_q1 = gaussian_elimination(A1, b1)
 
-def question4():
-    # Check positive definite
-    A = np.array([[2, 2, 1],
-                  [2, 3, 0],
-                  [1, 0, 2]])
-    
-    # Check if symmetric
-    if not np.allclose(A, A.T):
-        print(False)
-        return
-    
-    # Check if all eigenvalues are positive
-    eigvals = np.linalg.eigvals(A)
-    is_pos_def = np.all(eigvals > 0)
-    print(is_pos_def)
+print(solution_q1[0])
+print("\n")
+print(solution_q1[1])
+print("\n")
+print(solution_q1)
+print("\n")
 
-if __name__ == "__main__":
-    question1()
-    question2()
-    question3()
-    question4()
-   
+# Question 2
+A2 = np.array([[1, 1, 0, 3], [2, 1, -1, 1], [3, -1, -1, 2], [-1, 2, 3, -1]], dtype=float)
+L2, U2 = lu_factorization(A2)
+det_A2 = np.prod(np.diag(U2))
+
+# Print exactly as shown in expected output
+print(det_A2)
+print("\n")
+print(L2)
+print("\n")
+print(U2)
+
